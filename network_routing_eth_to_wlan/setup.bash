26eb44ae70ff9f95219b10944ac3f6927ffa85de
Tatsuhiro Ikebe <beike315@icloud.com>
2024-11-03 18:33:08 +0900

    fix: search ET_NIC_NAME

--- network_routing_eth_to_wlan/setup.bash
+++ network_routing_eth_to_wlan/setup.bash
@@ -1,7 +1,7 @@
 #!/bin/bash -evx
 
-export ET_NIC_NAME=$(ip -o link show | awk -F': ' '$2 ~ /^en[opsx]/ && $9 == "UP" {print $2}')
-export WL_NIC_NAME=$(ip -o link show | awk -F': ' '$2 ~ /^wl[opsx]/ && $9 == "UP" {print $2}')
+export ET_NIC_NAME=$(ip -o link show | awk -F': ' '/^[0-9]+: en[opsx]/ && /state UP/ {print $2}')
+export WL_NIC_NAME=$(ip -o link show | awk -F': ' '/^[0-9]+: wl[opsx]/ && /state UP/ {print $2}')
 sudo iptables -t nat -A POSTROUTING -o $WL_NIC_NAME -j MASQUERADE
 sudo iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 sudo iptables -A FORWARD -i $ET_NIC_NAME -o $WL_NIC_NAME -j ACCEPT
